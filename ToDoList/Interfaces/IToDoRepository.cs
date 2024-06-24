4df2ff9a3833ef1758ea414dcba573c195c25d55
NicoResta01 <nresta@studenti.apuliadigitalmaker.it>
2024-06-24 15:03:56 +0200

    Primo commit

---
+++ ToDoList/Interfaces/IToDoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Interfaces
+{
+    public interface IToDoRepository
+    {
+        void Add(ToDoItem item);
+        bool Remove(int id);
+        List<ToDoItem> GetAll();
+
+    }
+}
---
+++ ToDoList/ToDoRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Interfaces;
+
+namespace ToDoList
+{
+    public class ToDoRepository : IToDoRepository
+    {
+        private readonly List<ToDoItem> _toDoList = new List<ToDoItem>();
+        private int _nextId = 1;
+
+        public void Add(ToDoItem item)
+        {
+            item.Id = _nextId++;
+            _toDoList.Add(item);
+        }
+
+        public bool Remove(int id)
+        {
+            var item = _toDoList.FirstOrDefault(t => t.Id == id);
+            if (item != null)
+            {
+                _toDoList.Remove(item);
+                return true;
+            }
+            return false;
+        }
+
+        public List<ToDoItem> GetAll()
+        {
+            return _toDoList.ToList();
+        }
+    }
+}
